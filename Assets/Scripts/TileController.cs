eeaf7cc11d105660a6eae4cb18ed3ecaa83f39db
j-weissen <jweissen@htl-steyr.ac.at>
2022-05-06 11:55:07 +0200

    Added GrassTile Prefab and Simple TileController

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,26 @@ using UnityEngine;
 public class TileController : MonoBehaviour
 {
     public GameObject tile;
+
+    public GameObject CameraGameObject;
+     
     // Start is called before the first frame update
     void Start()
     {
-        for (int x = 0; x < 10; x++)
+        Camera camera = CameraGameObject.GetComponent<Camera>();
+        Vector3 screen = camera.ViewportToWorldPoint(new Vector3(1,1,camera.nearClipPlane));
+        int x = Convert.ToInt32(Math.Ceiling(screen.x));
+        int y = Convert.ToInt32(Math.Ceiling(screen.y));
+        
+        Debug.Log(screen);
+        for (int xx = -x; xx <= x; xx++)
         {
-            for (int y = 0; y < 10; y++)
+            for (int yy = -y; yy <= y; yy++)
             {
-                //Instantiate(tile, new Vector3(x, y, 0), Quaternion.identity);
+                if (tile != null)
+                {
+                    Instantiate(tile, new Vector3(xx, yy, 0), Quaternion.identity);
+                }
             }
         }
     }
