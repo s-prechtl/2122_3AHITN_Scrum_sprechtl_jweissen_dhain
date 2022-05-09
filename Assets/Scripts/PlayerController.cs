d84a815d7439eb044c32fff5535c8c2276e70b4f
s-prechtl <sprechtl@htl-steyr.ac.at>
2022-05-09 23:38:56 +0200

    UsableItem.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    private List<Item> inventory;
+    private int money;
+    private UsableItem selectedItem;
     
+    private static PlayerController instance;
+    
+    public int startMoney = 100;
+
+    public static PlayerController getInstance() {
+        return instance;
+    }
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventory ??= new List<Item>();
+        money = startMoney;
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void setSelectedItem(UsableItem item) {
+        selectedItem = item;
+        
         
     }
 }
