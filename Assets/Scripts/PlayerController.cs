3466d25dbb22ef41736b02227fb61292c98b0f5a
s-prechtl <sprechtl@htl-steyr.ac.at>
2022-05-10 00:01:21 +0200

    PlayerController.cs select Item

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    private List<Item> inventory;
+    private Dictionary<Item, int> inventory;
     private int money;
     private UsableItem selectedItem;
     
@@ -19,7 +20,7 @@ public class PlayerController : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        inventory ??= new List<Item>();
+        inventory ??= new Dictionary<Item, int>();
         money = startMoney;
         instance = this;
     }
@@ -31,8 +32,11 @@ public class PlayerController : MonoBehaviour {
     }
 
     public void setSelectedItem(UsableItem item) {
-        selectedItem = item;
-        
-        
+        if (inventory.ContainsKey(item)) {
+            selectedItem = item;
+            Cursor.SetCursor(item.defaultSprite.texture,  Vector2.zero, CursorMode.Auto);
+        } else {
+          Debug.Log("An item requested to select isn't in the inventory" + item);  
+        }
     }
 }
