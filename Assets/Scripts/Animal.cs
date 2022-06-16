2a29e5413b54042e470531cff997d54f7eac5409
d-hain <d.hain@gmx.at>
2022-06-16 15:25:54 +0200

    Animals can now move and stop and be animated to move cow now has idle and run animation

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -6,14 +6,17 @@ using Random = UnityEngine.Random;
 
 public class Animal : MonoBehaviour {
     private Rigidbody2D _rigidbody;
+    private int _animMoveID;
     
     public Item producedItem;
     public Sprite animalSprite;
     public int movementSpeed;
+    public Animator animator;
     
     private void Start() {
         _rigidbody = gameObject.GetComponent<Rigidbody2D>();
         animalSprite = gameObject.GetComponent<SpriteRenderer>().GetComponent<Sprite>();
+        _animMoveID = Animator.StringToHash("moving");
         
         // Move the Animal in any random direction every 1-5s
         InvokeRepeating(nameof(MoveInRandomDirection), 2f, Random.Range(1f, 5f));
@@ -31,14 +34,14 @@ public class Animal : MonoBehaviour {
             direction.Normalize();
         
             _rigidbody.velocity = movementSpeed * direction;
-            StopAllCoroutines();
+            animator.SetBool(_animMoveID, true);
             yield return new WaitForSeconds(randTime);
-            _rigidbody.velocity = new Vector2(0f, 0f); }
+            _rigidbody.velocity = new Vector2(0f, 0f); 
+            animator.SetBool(_animMoveID, false);
+        }
         
         StartCoroutine(Move());
     }
-
-    // TODO: Animations
 
     private void OnMouseDown() {
         ActionInvoker.InvokeAction(gameObject, PlayerController.instance.SelectedItem);
